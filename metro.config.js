75a092f180c855f6c9abeb044a71859e7a0fdf6f
yongjun <dydwnsl02@naver.com>
2025-08-21 01:10:02 +0900

    fix: 포트변경 metro

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,9 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const config = {
+  server: {
+    port: 8082  // 포트 8082로 변경
+  }
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
